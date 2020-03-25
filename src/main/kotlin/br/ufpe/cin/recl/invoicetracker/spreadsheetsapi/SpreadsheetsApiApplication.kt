4f6d37af297c26063c3d5350038c514356fe4636
Ricardo Ebbers <recl@cin.ufpe.br>
2020-03-24 23:39:07 -0300

    Initialized Spring project

---
+++ src/main/kotlin/br/ufpe/cin/recl/invoicetracker/spreadsheetsapi/SpreadsheetsApiApplication.kt
@@ -0,0 +1,11 @@
+package br.ufpe.cin.recl.invoicetracker.spreadsheetsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpreadsheetsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpreadsheetsApiApplication>(*args)
+}
